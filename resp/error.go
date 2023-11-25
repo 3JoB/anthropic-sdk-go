101341a7379db6044150abafe6f4ae3e1b355642
Zero <54469706+3JoB@users.noreply.github.com>
2023-11-25 12:11:48 +0800

    resp: update

---
+++ resp/error.go
@@ -0,0 +1,60 @@
+package resp
+
+import (
+	"github.com/3JoB/unsafeConvert"
+	"github.com/sugawarayuuta/sonnet"
+)
+
+type ErrorResponse struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+	Code    int    `json:"-"` // HTTP status code, please do not parse
+}
+
+type R struct {
+	Error *ErrorResponse `json:"error"`
+}
+
+func Error(code int, v []byte) (*ErrorResponse, error) {
+	var e = R{
+		Error: &ErrorResponse{},
+	}
+	err := sonnet.Unmarshal(v, &e)
+	e.Error.Code = code
+	return e.Error, err
+}
+
+/*
+Example:
+
+	func E() error {
+		return &ErrorResponse{}
+	}
+*/
+func (e *ErrorResponse) Error() string {
+	return e.Message
+}
+
+// Return an error object
+func (e *ErrorResponse) Err() error {
+	return e
+}
+
+// Return HTTP status code
+func (e *ErrorResponse) StatusCode() int {
+	return e.Code
+}
+
+// Check if the HTTP status code matches the entered status code
+func (e *ErrorResponse) IsStatusCode(code int) bool {
+	return code == e.Code
+}
+
+// Return a String object
+//
+// if you need to call it multiple times, please assign or cache it directly,
+// because the structure will call json to decode itself
+func (e *ErrorResponse) String() string {
+	d, _ := sonnet.Marshal(e)
+	return unsafeConvert.StringSlice(d)
+}
